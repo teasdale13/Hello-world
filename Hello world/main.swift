ed169d3b3382c2239029e921f00898d43700079a
Kevin Teasdale Dube <kevin_teasdale_dube@hotmail.com>
2018-08-05 01:53:53 -0400

    Initial Commit

---
+++ Hello world/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hello world
+//
+//  Created by Kevin Teasdale Dube on 18-08-05.
+//  Copyright © 2018 Kevin Teasdale Dube. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
